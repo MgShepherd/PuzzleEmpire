e11ad7b40bb833f4673c22e9bf5b078eef2febbf
MgShepherd <michaelshep1504@gmail.com>
2023-09-30 11:27:35 +0100

    Setup jest for running tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders page title", () => {
   render(<App />);
   expect(screen.getByText("Puzzle Empire")).toBeInTheDocument();
 });
