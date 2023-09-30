530f3f908ba40f8abbf4f164df261af89f26f65a
MgShepherd <michaelshep1504@gmail.com>
2023-09-30 12:13:28 +0100

    Setup eslint and prettier for code style and formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders page title", () => {
+test('renders page title', () => {
   render(<App />);
-  expect(screen.getByText("Puzzle Empire")).toBeInTheDocument();
+  expect(screen.getByText('Puzzle Empire')).toBeInTheDocument();
 });
