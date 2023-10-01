b7330cb15346536a48e6cd229860935999336c26
MgShepherd <michaelshep1504@gmail.com>
2023-10-01 12:45:42 +0100

    Added tests for sudoku rendering

--- src/components/SudokuBlock/SudokuBlock.test.tsx
+++ src/components/SudokuBlock/SudokuBlock.test.tsx
@@ -3,19 +3,31 @@ import SudokuElement from '../../types/SudokuElement';
 import SudokuBlock from './SudokuBlock';
 
 describe('Sudoku Block tests', () => {
-  it('should render the correct number of squares', () => {
-    const elements: SudokuElement[] = [
+  let elements: SudokuElement[];
+
+  beforeEach(() => {
+    elements = [
       { value: '1' },
       { value: '1' },
       { value: '1' },
-      { value: '1' },
-      { value: '1' },
-      { value: '1' },
-      { value: '1' },
-      { value: '1' },
-      { value: '1' },
+      { value: '2' },
+      { value: '2' },
+      { value: '0' },
+      { value: '0' },
+      { value: '0' },
+      { value: '3' },
     ];
     render(<SudokuBlock elements={elements} />);
+  });
+
+  it('should render the correct number of squares', () => {
     expect(screen.getAllByTestId('sudokuSquare')).toHaveLength(elements.length);
+  });
+
+  it('should render the correct values for squares based on elements', () => {
+    expect(screen.getAllByText('1')).toHaveLength(3);
+    expect(screen.getAllByText('2')).toHaveLength(2);
+    expect(screen.getAllByText('3')).toHaveLength(1);
+    expect(screen.queryAllByText('4')).toHaveLength(0);
   });
 });
--- src/components/SudokuGrid/SudokuGrid.test.tsx
+++ src/components/SudokuGrid/SudokuGrid.test.tsx
@@ -8,4 +8,13 @@ describe('Sudoku Grid tests', () => {
     render(<SudokuGrid elementString={elementString} />);
     expect(screen.getAllByTestId('sudokuBlock')).toHaveLength(9);
   });
+
+  it('should render the correct values in each square based on the input string', () => {
+    const elementString =
+      '111111111,222222222,120456789,120456789,120456789,120456789,120456789,120456789,120456789';
+    render(<SudokuGrid elementString={elementString} />);
+    expect(screen.getAllByText('1')).toHaveLength(16);
+    expect(screen.queryAllByText('3')).toHaveLength(0);
+    expect(screen.getAllByText('9')).toHaveLength(7);
+  });
 });
