4e736f73b2fc2f0dc87bf2ef65f2ef4631051839
MgShepherd <michaelshep1504@gmail.com>
2023-10-14 21:01:53 +0100

    Improved rendering of sudoku square

--- src/components/SudokuSquare/SudokuSquare.tsx
+++ src/components/SudokuSquare/SudokuSquare.tsx
@@ -14,19 +14,22 @@ const SudokuSquare = ({ elementIndex }: SudokuSquareProps): JSX.Element => {
 
   const getSelectedStyle = (): string => {
     if (!element.selected) return '';
-    return 'ring-green-500 ring';
+    return 'ring-green-500 ring-4 ';
   };
 
   return (
     <div
       data-testid="sudokuSquare"
-      className="w-full h-full ring-black ring-1 flex justify-center items-center p"
+      className="w-full h-full ring-black ring-1 p-1"
       onClick={() => dispatch(selectSquare(elementIndex))}
     >
       {element !== undefined && (
         <div
           data-testid={element.selected ? 'selectedSquare' : ''}
-          className={getSelectedStyle() + ' w-full h-full'}
+          className={
+            getSelectedStyle() +
+            ' w-full h-full flex justify-center items-center text-3xl'
+          }
         >
           {element.value !== '0' ? element.value : ''}
         </div>
